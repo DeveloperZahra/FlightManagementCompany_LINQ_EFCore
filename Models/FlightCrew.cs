ea0503e0c68f1026f1615cb18b5804993579c9fa
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 16:11:28 +0400

    Composite Primary Key

--- Models/FlightCrew.cs
+++ Models/FlightCrew.cs
@@ -8,5 +8,8 @@ namespace FlightManagementCompany.Models
 {
     public class FlightCrew
     {
+       // Composite Primary Key 
+        public int FlightId { get; set; }
+        public int CrewId { get; set; }
     }
 }
