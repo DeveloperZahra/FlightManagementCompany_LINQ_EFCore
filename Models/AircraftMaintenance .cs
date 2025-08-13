b9ba641f0b7879bb281c85933ab51838fe77f940
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 16:06:31 +0400

    Primary Key for the Maintenance entity

--- Models/AircraftMaintenance .cs
+++ Models/AircraftMaintenance .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace FlightManagementCompany.Models
 {
     public class AircraftMaintenance
     {
+        // Primary Key for the Maintenance entity
+        [Key]
+        public int MaintenanceId { get; set; } 
+
+        // Date of maintenance
+        [Required]
+        public DateTime MaintenanceDate { get; set; } // [cite: 65]
+
+        // Type of maintenance (e.g., A-Check, B-Check)
+        [Required]
+        public string Type { get; set; } // [cite: 66]
+
+        // Notes on the maintenance performed
+        public string? Notes { get; set; } // [cite: 67]
+
     }
 }
