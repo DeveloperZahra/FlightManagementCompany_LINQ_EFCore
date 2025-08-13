9a2202cfd43ac6e762a0d7e68ea295cefd323d5c
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 15:04:03 +0400

    Foreign Key to the Route

--- Models/Flight .cs
+++ Models/Flight .cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,12 @@ namespace FlightManagementCompany.Models
 
         // Current status of the flight
         [Required]
-        public string Status { get; set; } 
+        public string Status { get; set; }
+
+
+        // Foreign Key to the Route
+        public int RouteId { get; set; }
+        [ForeignKey("RouteId")]
+        public Route Route { get; set; }
     }
 }
