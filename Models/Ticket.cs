84ac92da8cd2d7fae8c9e0e0682f69ee483c3871
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 15:49:46 +0400

    Primary Key for the Ticket entity

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace FlightManagementCompany.Models
 {
     public class Ticket
     {
+        // Primary Key for the Ticket entity
+        [Key]
+        public int TicketId { get; set; } 
+
+        // Seat number on the flight
+        [Required]
+        public string SeatNumber { get; set; } // [cite: 52]
+
+        // Fare for the ticket
+        [Required]
+        [cite_start]
+        [Column(TypeName = "decimal(18, 2)")] // Specify decimal precision 
+        public decimal Fare { get; set; } // [cite: 53]
+
+        // Check-in status
+        [Required]
+        public bool Checkedin { get; set; } // [cite: 54]
     }
 }
