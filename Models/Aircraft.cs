c5b3b1ad3eef82e2bd6d25dbd32b64afac65fc02
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 11:47:26 +0400

    Primary Key for the Aircraft entity

--- Models/Aircraft.cs
+++ Models/Aircraft.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace FlightManagementCompany.Models
 {
     public class Aircraft
     {
+        // Primary Key for the Aircraft entity
+        [Key]
+        public int AircraftId { get; set; } 
+
+        // Unique tail number of the aircraft
+        [Required]
+        public string TailNumber { get; set; } // [cite: 21]
+
+        // Model of the aircraft
+        [Required]
+        public string Model { get; set; } // [cite: 22]
+
+        // Seating capacity of the aircraft
+        [Required]
+        public int Capacity { get; set; } // [cite: 23]
     }
 }
