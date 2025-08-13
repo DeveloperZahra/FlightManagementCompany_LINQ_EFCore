68f495981b90150e494c51550fabde5af0274cec
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 11:53:27 +0400

    Primary Key for the CrewMember entity

--- Models/CrewMember.cs
+++ Models/CrewMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace FlightManagementCompany.Models
 {
     public class CrewMember
     {
+        // Primary Key for the CrewMember entity
+        [Key]
+        public int CrewId { get; set; } 
+
+        // Full name of the crew member
+        [Required]
+        public string FullName { get; set; } // [cite: 26]
+
+        // Role of the crew member (e.g., Pilot, CoPilot, FlightAttendant)
+        [Required]
+        public string Role { get; set; } // [cite: 27]
+
+        // License number, nullable for non-pilot roles
+        public string? LicenseNo { get; set; } // [cite: 28]
+
     }
 }
