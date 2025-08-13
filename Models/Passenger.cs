94c698fff9d157ea54d5c7947e675a38c938cd72
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 15:11:27 +0400

    Primary Key for the Passenger entity

--- Models/Passenger.cs
+++ Models/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace FlightManagementCompany.Models
 {
     public class Passenger
     {
+        // Primary Key for the Passenger entity
+        [Key]
+        public int PassengerId { get; set; } 
+
+        // Full name of the passenger
+        [Required]
+        public string FullName { get; set; } // [cite: 41]
+
+        // Unique passport number
+        [Required]
+        public string PassportNo { get; set; } // [cite: 42]
+
+        // Nationality of the passenger
+        [Required]
+        public string Nationality { get; set; } // [cite: 43]
+
+        // Date of Birth
+        [Required]
+        public DateTime DOB { get; set; } // [cite: 44]
     }
 }
