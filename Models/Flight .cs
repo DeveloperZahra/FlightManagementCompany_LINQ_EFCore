1369666359a06125964cc731688e37f94afac508
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 15:00:28 +0400

    Primary Key for the Flight entity

--- Models/Flight .cs
+++ Models/Flight .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace FlightManagementCompany.Models
 {
     public class Flight
     {
+        // Primary Key for the Flight entity
+        [Key]
+        public int FlightId { get; set; } 
+
+        // Flight number (e.g., "FM101")
+        [Required]
+        public string FlightNumber { get; set; } // [cite: 34]
+
+        // Departure time in UTC
+        [Required]
+        public DateTime DepartureUtc { get; set; } // [cite: 35]
+
+        // Arrival time in UTC
+        [Required]
+        public DateTime ArrivalUtc { get; set; } // [cite: 36]
+
+        // Current status of the flight
+        [Required]
+        public string Status { get; set; } // [cite: 37]
     }
 }
