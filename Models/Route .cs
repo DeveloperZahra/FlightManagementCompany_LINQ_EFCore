b62d647d0df7c444039a5aac4ca11b06da791b5b
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-13 11:58:52 +0400

    Primary Key for the Route entity

--- Models/Route .cs
+++ Models/Route .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace FlightManagementCompany.Models
 {
     public class Route
     {
+        // Primary Key for the Route entity
+        [Key]
+        public int RouteId { get; set; } 
+
+        // Distance in kilometers
+        [Required]
+        public int DistanceKm { get; set; } // [cite: 32]
+
+
     }
 }
