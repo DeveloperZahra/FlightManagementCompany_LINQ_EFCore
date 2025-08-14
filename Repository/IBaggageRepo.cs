1e5ae6430861679791abfef94256c5796e2b626f
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 14:37:19 +0400

    Adds a new baggage record to the underlying data store

--- Repository/IBaggageRepo.cs
+++ Repository/IBaggageRepo.cs
@@ -6,7 +6,7 @@ namespace FlightManagementCompany.Repository
 /// It specifies the core data access operations (CRUD) for the Baggage model
     public interface IBaggageRepo
     {
-        void AddBaggage(Baggage baggage);
+        void AddBaggage(Baggage baggage); // Adds a new baggage record to the underlying data store
         void DeleteBaggage(int BaggageId);
         IEnumerable<Baggage> GetAllBaggages();
         Baggage GetBaggageById(int BaggageId);
