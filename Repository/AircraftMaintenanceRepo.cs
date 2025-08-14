ab6129a4fee35f22a8f290ce0038d98bfce90e1f
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 11:15:32 +0400

    This repository class is responsible for all data access operations related to  the AircraftMaintenance entity

--- Repository/AircraftMaintenanceRepo.cs
+++ Repository/AircraftMaintenanceRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompany.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,52 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Repository
 {
+    //This repository class is responsible for all data access operations related to
+/// the AircraftMaintenance entity
     public class AircraftMaintenanceRepo
     {
+        private readonly FlightDbContext _context;
+
+        public AircraftMaintenanceRepo(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieve all maintenance records
+        public IEnumerable<AircraftMaintenance> GetAllAircraftMaintenance()
+        {
+            return _context.AircraftMaintenances.ToList();
+        }
+
+        // Retrieve a maintenance record by ID
+        public AircraftMaintenance GetAircraftMaintenanceById(int MaintenanceId)
+        {
+            return _context.AircraftMaintenances.Find(MaintenanceId);
+        }
+
+        // Add a new maintenance record
+        public void AddAircraftMaintenance(AircraftMaintenance maintenance)
+        {
+            _context.AircraftMaintenances.Add(maintenance);
+            _context.SaveChanges();
+        }
+
+        // Update an existing maintenance record
+        public void UpdateAircraftMaintenance(AircraftMaintenance maintenance)
+        {
+            _context.AircraftMaintenances.Update(maintenance);
+            _context.SaveChanges();
+        }
+
+        // Delete a maintenance record by ID
+        public void DeleteAircraftMaintenance(int MaintenanceId)
+        {
+            var maintenance = _context.AircraftMaintenances.Find(MaintenanceId);
+            if (maintenance != null)
+            {
+                _context.AircraftMaintenances.Remove(maintenance);
+                _context.SaveChanges();
+            }
+        }
     }
 }
