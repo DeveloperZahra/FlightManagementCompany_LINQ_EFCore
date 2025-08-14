a8639bbf3fc277147b434701a8be919bbb4092d8
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 15:31:10 +0400

    Adds a new crew member to the underlying data store

--- Repository/ICrewMemberRepo.cs
+++ Repository/ICrewMemberRepo.cs
@@ -2,9 +2,11 @@
 
 namespace FlightManagementCompany.Repository
 {
+    //This interface defines the contract for a repository that manages CrewMember entities.
+/// It specifies the core data access operations (CRUD) for the CrewMember model.
     public interface ICrewMemberRepo
     {
-        void AddCrewMember(CrewMember crewMember);
+        void AddCrewMember(CrewMember crewMember);// Adds a new crew member to the underlying data store
         void DeleteCrewMember(int CrewId);
         IEnumerable<CrewMember> GetAllCrewMembers();
         CrewMember GetCrewMemberById(int CrewId);
