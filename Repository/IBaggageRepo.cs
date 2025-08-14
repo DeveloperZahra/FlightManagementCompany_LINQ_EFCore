35d090aed21e1b20b6d59c82f8f70aa2f5571880
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 14:41:36 +0400

    Retrieves a single baggage record by its unique ID

--- Repository/IBaggageRepo.cs
+++ Repository/IBaggageRepo.cs
@@ -9,7 +9,7 @@ namespace FlightManagementCompany.Repository
         void AddBaggage(Baggage baggage); // Adds a new baggage record to the underlying data store
         void DeleteBaggage(int BaggageId); //eletes a baggage record from the data store based on its unique ID
         IEnumerable<Baggage> GetAllBaggages();//Retrieves all baggage records from the data store
-        Baggage GetBaggageById(int BaggageId);
+        Baggage GetBaggageById(int BaggageId); //Retrieves a single baggage record by its unique ID
         void UpdateBaggage(Baggage baggage);
     }
 }
