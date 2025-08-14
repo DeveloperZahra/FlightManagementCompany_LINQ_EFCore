59b2fa4d63a4b8c2057c636d05445b29ecbb6bc7
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 09:54:58 +0400

    // Private field to hold the database context.     // The FlightDbContext is used to interact with the database

--- Repository/AircraftRepo.cs
+++ Repository/AircraftRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompany.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,50 @@ namespace FlightManagementCompany.Repository
 {
     public class AircraftRepo
     {
+        // Private field to hold the database context.
+        // The FlightDbContext is used to interact with the database
+        private readonly FlightDbContext _context;
+
+        public AircraftRepo(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieve all aircrafts from the database
+        public IEnumerable<Aircraft> GetAll()
+        {
+            return _context.Aircrafts.ToList();
+        }
+
+        // Retrieve a single aircraft by ID
+        public Aircraft GetAircraftById(int AircraftId)
+        {
+            return _context.Aircrafts.Find(AircraftId);
+        }
+
+        // Add a new aircraft to the database
+        public void AddAircraft(Aircraft aircraft)
+        {
+            _context.Aircrafts.Add(aircraft);
+            _context.SaveChanges();
+        }
+
+        // Update an existing aircraft in the database
+        public void UpdateAircraft(Aircraft aircraft)
+        {
+            _context.Aircrafts.Update(aircraft);
+            _context.SaveChanges();
+        }
+
+        // Delete an aircraft by ID
+        public void DeleteAircraft(int AircraftId)
+        {
+            var aircraft = _context.Aircrafts.Find(AircraftId);
+            if (aircraft != null)
+            {
+                _context.Aircrafts.Remove(aircraft);
+                _context.SaveChanges();
+            }
+        }
     }
 }
