92b31842465bfd5d5e8522726dd7a1eba902d75b
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 15:14:10 +0400

    This repository class is responsible for all data access operations related to  the CrewMember entity. It provides methods for performing CRUD operations.

--- Repository/CrewMemberRepo.cs
+++ Repository/CrewMemberRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompany.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,52 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Repository
 {
+    //This repository class is responsible for all data access operations related to
+/// the CrewMember entity. It provides methods for performing CRUD operations.
     public class CrewMemberRepo
     {
+        private readonly FlightDbContext _context;
+
+        public CrewMemberRepo(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieve all crew members
+        public IEnumerable<CrewMember> GetAllCrewMembers()
+        {
+            return _context.CrewMembers.ToList();
+        }
+
+        // Retrieve a crew member by ID
+        public CrewMember GetCrewMemberById(int CrewId)
+        {
+            return _context.CrewMembers.Find(CrewId);
+        }
+
+        // Add a new crew member
+        public void AddCrewMember(CrewMember crewMember)
+        {
+            _context.CrewMembers.Add(crewMember);
+            _context.SaveChanges();
+        }
+
+        // Update an existing crew member
+        public void UpdateCrewMember(CrewMember crewMember)
+        {
+            _context.CrewMembers.Update(crewMember);
+            _context.SaveChanges();
+        }
+
+        // Delete a crew member by ID
+        public void DeleteCrewMember(int CrewId)
+        {
+            var crewMember = _context.CrewMembers.Find(CrewId);
+            if (crewMember != null)
+            {
+                _context.CrewMembers.Remove(crewMember);
+                _context.SaveChanges();
+            }
+        }
     }
 }
