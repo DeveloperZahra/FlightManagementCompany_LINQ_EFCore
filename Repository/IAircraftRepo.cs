cfe7373950d1db244e305a6944bc1668a294fc74
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 10:12:19 +0400

    Retrieves a single aircraft by its unique ID

--- Repository/IAircraftRepo.cs
+++ Repository/IAircraftRepo.cs
@@ -2,6 +2,7 @@
 using static System.Formats.Asn1.AsnWriter;
 using System.Xml;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
 
 namespace FlightManagementCompany.Repository
 {
@@ -11,10 +12,8 @@ namespace FlightManagementCompany.Repository
     {
        // Adds a new aircraft to the underlying data store
        void AddAircraft(Aircraft aircraft); //The Aircraft object to be added
-
-        //Deletes an aircraft from the data store based on its unique ID
-        void DeleteAircraft(int AircraftId);
-        Aircraft GetAircraftById(int AircraftId);
+       void DeleteAircraft(int AircraftId); //Deletes an aircraft from the data store based on its unique ID
+        Aircraft GetAircraftById(int AircraftId); //Retrieves a single aircraft by its unique ID
         IEnumerable<Aircraft> GetAllAircraft();
         void UpdateAircraft(Aircraft aircraft);
     }
