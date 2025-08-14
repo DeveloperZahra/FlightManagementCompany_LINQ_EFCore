0c476d58336dd0eeea2c597342b1cadac0177b03
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 10:07:41 +0400

    This interface defines the contract for a repository that manages Aircraft entities.  It specifies the core data access operations (CRUD) for the Aircraft model

---
+++ Repository/IAircraftRepo.cs
@@ -0,0 +1,15 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+   // This interface defines the contract for a repository that manages Aircraft entities.
+/// It specifies the core data access operations (CRUD) for the Aircraft model
+    public interface IAircraftRepo
+    {
+        void AddAircraft(Aircraft aircraft);
+        void DeleteAircraft(int AircraftId);
+        Aircraft GetAircraftById(int AircraftId);
+        IEnumerable<Aircraft> GetAllAircraft();
+        void UpdateAircraft(Aircraft aircraft);
+    }
+}
