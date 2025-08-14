e12b9dda9a441436bdbc30d6c475c423bde2690a
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 10:08:50 +0400

    Adds a new aircraft to the underlying data store

--- Repository/IAircraftRepo.cs
+++ Repository/IAircraftRepo.cs
@@ -1,11 +1,14 @@
 ﻿using FlightManagementCompany.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlightManagementCompany.Repository
 {
-   // This interface defines the contract for a repository that manages Aircraft entities.
-/// It specifies the core data access operations (CRUD) for the Aircraft model
+    // This interface defines the contract for a repository that manages Aircraft entities.
+    /// It specifies the core data access operations (CRUD) for the Aircraft model
     public interface IAircraftRepo
     {
+       // Adds a new aircraft to the underlying data store
+
         void AddAircraft(Aircraft aircraft);
         void DeleteAircraft(int AircraftId);
         Aircraft GetAircraftById(int AircraftId);
