0859d07be3009d75321e29fa3547019468a3023c
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 12:12:52 +0400

    Adds a new airport to the underlying data store

--- Repository/IAirportRepo.cs
+++ Repository/IAirportRepo.cs
@@ -1,12 +1,13 @@
 ﻿using FlightManagementCompany.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlightManagementCompany.Repository
 {
     //This interface defines the contract for a repository that manages Airport entities.
-/// It specifies the core data access operations (CRUD) for the Airport model
+    /// It specifies the core data access operations (CRUD) for the Airport model
     public interface IAirportRepo
     {
-        void AddAirport(Airport airport);
+        void AddAirport(Airport airport); //Adds a new airport to the underlying data store
         void DeleteAirport(int AirportId);
         Airport GetAirportById(int AirportId);
         IEnumerable<Airport> GetAllAirports();
