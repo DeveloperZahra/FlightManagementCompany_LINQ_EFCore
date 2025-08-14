a9cab192966b5923af164146aa26ea25fb1a1c3a
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 14:33:39 +0400

    This interface defines the contract for a repository that manages Baggage entities. It specifies the core data access operations (CRUD) for the Baggage model

---
+++ Repository/IBaggageRepo.cs
@@ -0,0 +1,15 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+   // This interface defines the contract for a repository that manages Baggage entities.
+/// It specifies the core data access operations (CRUD) for the Baggage model
+    public interface IBaggageRepo
+    {
+        void AddBaggage(Baggage baggage);
+        void DeleteBaggage(int BaggageId);
+        IEnumerable<Baggage> GetAllBaggages();
+        Baggage GetBaggageById(int BaggageId);
+        void UpdateBaggage(Baggage baggage);
+    }
+}
