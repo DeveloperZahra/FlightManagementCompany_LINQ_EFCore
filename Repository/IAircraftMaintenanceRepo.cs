5991a29ae3bf2ddda81a545ffc6849cad64cd606
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-14 11:28:18 +0400

    Retrieves a single aircraft maintenance record by its unique ID

--- Repository/IAircraftMaintenanceRepo.cs
+++ Repository/IAircraftMaintenanceRepo.cs
@@ -8,7 +8,7 @@ namespace FlightManagementCompany.Repository
     {
         void AddAircraftMaintenance(AircraftMaintenance maintenance);//Adds a new aircraft maintenance record to the underlying data store
         void DeleteAircraftMaintenance(int MaintenanceId); //Deletes an aircraft maintenance record from the data store based on its unique ID.
-        AircraftMaintenance GetAircraftMaintenanceById(int MaintenanceId);
+        AircraftMaintenance GetAircraftMaintenanceById(int MaintenanceId); //Retrieves a single aircraft maintenance record by its unique ID
         IEnumerable<AircraftMaintenance> GetAllAircraftMaintenance();
         void UpdateAircraftMaintenance(AircraftMaintenance maintenance);
     }
