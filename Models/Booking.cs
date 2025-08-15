e5f04f41a2903837c2940e08ac7427d07f1a5c62
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 07:04:35 +0400

    last update in booking class

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -34,6 +34,6 @@ namespace FlightManagementCompany.Models
 
 
         // Navigation property for tickets in this booking
-        public ICollection<Ticket> Tickets { get; set; }
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
