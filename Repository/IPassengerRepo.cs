8ee0e3589f3f676c753913566b8940ee615b7b17
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:39:45 +0400

    This interface defines the contract for a repository that manages Passenger entities. It specifies the core data access operations (CRUD) for the Passenger model.

---
+++ Repository/IPassengerRepo.cs
@@ -0,0 +1,15 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+   // This interface defines the contract for a repository that manages Passenger entities.
+/// It specifies the core data access operations (CRUD) for the Passenger model.
+    public interface IPassengerRepo
+    {
+        void Addpassenger(Passenger passenger);
+        void Deletepassenger(int PassengerId);
+        IEnumerable<Passenger> GetAllPassengers();
+        Passenger GetpassengerById(int PassengerId);
+        void UpdatePassenger(Passenger passenger);
+    }
+}
--- Repository/PassengerRepo.cs
+++ Repository/PassengerRepo.cs
@@ -8,19 +8,19 @@ using System.Threading.Tasks;
 namespace FlightManagementCompany.Repository
 {
     //This repository class handles all data access operations for the Passenger entity.
-/// It provides a connection to the database context to perform these operations
-    public class PassengerRepo
+    /// It provides a connection to the database context to perform these operations
+    public class PassengerRepo : IPassengerRepo
     {
         // A private, read-only field to hold the database context.
         // This is the bridge between the application's models and the database
         private readonly FlightDbContext _context;
 
         //Initializes a new instance of the PassengerRepository class.
-    /// This constructor receives the database context via dependency injection
-            public PassengerRepo(FlightDbContext context)
-            {
-                _context = context;
-            }
+        /// This constructor receives the database context via dependency injection
+        public PassengerRepo(FlightDbContext context)
+        {
+            _context = context;
+        }
 
         // Retrieve all passengers
         public IEnumerable<Passenger> GetAllPassengers()
@@ -57,9 +57,7 @@ namespace FlightManagementCompany.Repository
                 _context.Passengers.Remove(passenger);
                 _context.SaveChanges();
             }
-
-
-
-
         }
+
+    }
 }
