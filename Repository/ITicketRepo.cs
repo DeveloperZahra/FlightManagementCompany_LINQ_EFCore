a171595152fe69f8b4d6462a859ffc30a8a553d6
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 09:05:50 +0400

    This interface defines the contract for a repository that manages Ticket entities.  It specifies the core data access operations (CRUD) for the Ticket model

---
+++ Repository/ITicketRepo.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface ITicketRepo
+    {
+        void AddTicket(Ticket ticket);
+        void DeleteTicket(int TicketId);
+        IEnumerable<Ticket> GetAllTickets();
+        Ticket GetTicketById(int TicketId);
+        void UpdateTicket(Ticket ticket);
+    }
+}
--- Repository/TicketRepo.cs
+++ Repository/TicketRepo.cs
@@ -9,18 +9,18 @@ namespace FlightManagementCompany.Repository
 {
     //This repository class handles all data access operations for the Ticket entity.
     /// It provides a connection to the database context to perform these operations.
-    public class TicketRepo
+    public class TicketRepo : ITicketRepo
     {
         // A private, read-only field to hold the database context.
         // This is the bridge between the application's models and the database.
         private readonly FlightDbContext _context;
 
         //Initializes a new instance of the TicketRepository class.
-    /// This constructor receives the database context via dependency injection
-            public TicketRepo(FlightDbContext context)
-            {
-                _context = context;
-            }
+        /// This constructor receives the database context via dependency injection
+        public TicketRepo(FlightDbContext context)
+        {
+            _context = context;
+        }
 
         // Retrieve all tickets
         public IEnumerable<Ticket> GetAllTickets()
