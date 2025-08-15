7d6c9e13a0d66ed6f6f6438e1f943641b5f0b837
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 09:10:22 +0400

    Deletes a flight crew association from the data store based on its unique ID.

--- Repository/IFlightCrewRepo.cs
+++ Repository/IFlightCrewRepo.cs
@@ -9,7 +9,7 @@ namespace FlightManagementCompany.Repository
     {
         void AddFlightCrew(FlightCrew flightCrew);//Adds a new flight crew association to the underlying data store
         void DeleteFlightCrew(int id);//Adds a new flight crew association to the underlying data store
-        IEnumerable<FlightCrew> GetAllFlightCrews();
+        IEnumerable<FlightCrew> GetAllFlightCrews();//Deletes a flight crew association from the data store based on its unique ID.
         FlightCrew GetFlightCrewById(int id);
         void UpdateFlightCrew(FlightCrew flightCrew);
     }
