ebe8a5c99e73db8f1277211457e89cdc186c2fb4
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:57:06 +0400

    Adds a new route to the underlying data store

--- Repository/IRouteRepo.cs
+++ Repository/IRouteRepo.cs
@@ -2,9 +2,11 @@
 
 namespace FlightManagementCompany.Repository
 {
+   // This interface defines the contract for a repository that manages Route entities.
+/// It specifies the core data access operations (CRUD) for the Route model.
     public interface IRouteRepo
     {
-        void AddRoute(Route route);
+        void AddRoute(Route route);// Adds a new route to the underlying data store
         void DeleteRoute(int RouteId);
         IEnumerable<Route> GetAllRoutes();
         Route GetRouteById(int RouteId);
