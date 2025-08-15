868fb83659734e7984f8584a07d27dce04e00d54
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:46:17 +0400

    Retrieves all passengers from the data store.

--- Repository/IPassengerRepo.cs
+++ Repository/IPassengerRepo.cs
@@ -8,7 +8,7 @@ namespace FlightManagementCompany.Repository
     {
         void Addpassenger(Passenger passenger);//Adds a new passenger to the underlying data store
         void Deletepassenger(int PassengerId);//Deletes a passenger from the data store based on their unique ID
-        IEnumerable<Passenger> GetAllPassengers();
+        IEnumerable<Passenger> GetAllPassengers();//Retrieves all passengers from the data store.
         Passenger GetpassengerById(int PassengerId);
         void UpdatePassenger(Passenger passenger);
     }
