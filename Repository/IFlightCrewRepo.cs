d23ecee42c090f341d80b7ca5b0ee3f1ae54086d
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 09:11:51 +0400

    Retrieves a single flight crew association by its unique ID

--- Repository/IFlightCrewRepo.cs
+++ Repository/IFlightCrewRepo.cs
@@ -8,9 +8,9 @@ namespace FlightManagementCompany.Repository
     public interface IFlightCrewRepo
     {
         void AddFlightCrew(FlightCrew flightCrew);//Adds a new flight crew association to the underlying data store
-        void DeleteFlightCrew(int id);//Adds a new flight crew association to the underlying data store
-        IEnumerable<FlightCrew> GetAllFlightCrews();//Deletes a flight crew association from the data store based on its unique ID.
-        FlightCrew GetFlightCrewById(int id);//Retrieves all flight crew associations from the data store
-        void UpdateFlightCrew(FlightCrew flightCrew);
+        void DeleteFlightCrew(int id);//Deletes a flight crew association from the data store based on its unique ID
+        IEnumerable<FlightCrew> GetAllFlightCrews();//Retrieves all flight crew associations from the data store.
+        FlightCrew GetFlightCrewById(int id);//Retrieves a single flight crew association by its unique ID
+        void UpdateFlightCrew(FlightCrew flightCrew);//
     }
 }
