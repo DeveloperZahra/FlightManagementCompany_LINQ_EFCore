c1c0129b954ae8c3ddff7ca5815261b073dbaea5
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:50:09 +0400

    This repository class handles all data access operations for the Route entity. /// It provides a connection to the database context to perform these operations

--- Repository/RouteRepo .cs
+++ Repository/RouteRepo .cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Repository
 {
+    //This repository class handles all data access operations for the Route entity.
+/// It provides a connection to the database context to perform these operations
     public class RouteRepo
     {
+        
+            private readonly FlightDbContext _context;
+
+            public RouteRepo(FlightDbContext context)
+            {
+                _context = context;
+            }
+
+
     }
 }
