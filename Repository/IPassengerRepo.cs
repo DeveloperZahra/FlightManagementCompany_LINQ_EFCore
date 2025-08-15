2eda704d5c6873c817d555e3cbbd5eb65501df3c
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:44:37 +0400

    Deletes a passenger from the data store based on their unique ID

--- Repository/IPassengerRepo.cs
+++ Repository/IPassengerRepo.cs
@@ -7,7 +7,7 @@ namespace FlightManagementCompany.Repository
     public interface IPassengerRepo
     {
         void Addpassenger(Passenger passenger);//Adds a new passenger to the underlying data store
-        void Deletepassenger(int PassengerId);
+        void Deletepassenger(int PassengerId);//Deletes a passenger from the data store based on their unique ID
         IEnumerable<Passenger> GetAllPassengers();
         Passenger GetpassengerById(int PassengerId);
         void UpdatePassenger(Passenger passenger);
