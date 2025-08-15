37f53e872db64c2dec82b3d63363d0b7b5f32753
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 09:06:15 +0400

    Adds a new ticket to the underlying data store

--- Repository/ITicketRepo.cs
+++ Repository/ITicketRepo.cs
@@ -2,9 +2,11 @@
 
 namespace FlightManagementCompany.Repository
 {
+    //This interface defines the contract for a repository that manages Ticket entities.
+    /// It specifies the core data access operations (CRUD) for the Ticket model
     public interface ITicketRepo
     {
-        void AddTicket(Ticket ticket);
+        void AddTicket(Ticket ticket);// Adds a new ticket to the underlying data store
         void DeleteTicket(int TicketId);
         IEnumerable<Ticket> GetAllTickets();
         Ticket GetTicketById(int TicketId);
