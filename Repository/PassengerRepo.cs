8d2fc025a9f3962486d22d275cae9e55ad2ed3da
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:32:40 +0400

    This repository class handles all data access operations for the Passenger entity. /// It provides a connection to the database context to perform these operations

--- Repository/PassengerRepo.cs
+++ Repository/PassengerRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompany.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Repository
 {
-    public  class PassengerRepo
+    //This repository class handles all data access operations for the Passenger entity.
+/// It provides a connection to the database context to perform these operations
+    public class PassengerRepo
     {
-    }
+            private readonly FlightDbContext _context;
+
+            public PassengerRepo(FlightDbContext context)
+            {
+                _context = context;
+            }
+
+
+
+
+
+
+
+
+        }
 }
