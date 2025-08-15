ff23ef17f72347c4133dfdc97890a7c2b83a202d
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-15 08:46:56 +0400

    Retrieves a single passenger by their unique ID

--- Repository/IPassengerRepo.cs
+++ Repository/IPassengerRepo.cs
@@ -9,7 +9,7 @@ namespace FlightManagementCompany.Repository
         void Addpassenger(Passenger passenger);//Adds a new passenger to the underlying data store
         void Deletepassenger(int PassengerId);//Deletes a passenger from the data store based on their unique ID
         IEnumerable<Passenger> GetAllPassengers();//Retrieves all passengers from the data store.
-        Passenger GetpassengerById(int PassengerId);
+        Passenger GetpassengerById(int PassengerId);//Retrieves a single passenger by their unique ID
         void UpdatePassenger(Passenger passenger);
     }
 }
