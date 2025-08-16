38a8044b2eeb576b8da41220ab5788e0cf06dd0b
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-16 09:34:41 +0400

    DbContext class responsible for interacting with the Flight Management database

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FlightManagementCompany.Models;
 
-namespace FlightManagementCompany
+namespace FlightManagementCompany_LINQ_EFCore
 {
-    public  class FlightDbContext
+    // DbContext class responsible for interacting with the Flight Management database
+    public class FlightDbContext : DbContext
     {
+
+
+
+
+
     }
 }
