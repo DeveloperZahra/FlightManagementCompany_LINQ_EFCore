2d32d39200364d3c048f7a8e2bc994dad5caacc9
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-16 09:40:36 +0400

    Table for Aircraft Maintenances

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -19,6 +19,7 @@ namespace FlightManagementCompany_LINQ_EFCore
 
         // DbSets represent tables in the database for each entity
         public DbSet<Aircraft> Aircrafts { get; set; }                // Table for Aircrafts
+        public DbSet<AircraftMaintenance> AircraftMaintenances { get; set; } // Table for Aircraft Maintenances
 
 
     }
