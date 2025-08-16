0290ae84e05dc14f32f84d6a61d7d86467c58430
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-16 09:46:09 +0400

    Table for Routes

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -27,6 +27,7 @@ namespace FlightManagementCompany_LINQ_EFCore
         public DbSet<Flight> Flights { get; set; }                    // Table for Flights
         public DbSet<FlightCrew> FlightCrews { get; set; }            // Table for Flight Crew assignments
         public DbSet<Passenger> Passengers { get; set; }              // Table for Passengers
+        public DbSet<Route> Routes { get; set; }                      // Table for Routes
 
 
     }
