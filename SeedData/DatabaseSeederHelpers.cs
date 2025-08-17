1ca795215fd6a1f4d939490ceaf99ea8c2ea46cf
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-17 14:25:01 +0400

    The repository for managing AircraftMaintenance entities

--- SeedData/DatabaseSeederHelpers.cs
+++ SeedData/DatabaseSeederHelpers.cs
@@ -26,7 +26,7 @@ namespace FlightManagementCompany.SeedData
             IBaggageRepo baggageRepo,//The repository for managing Baggage entities
             ICrewMemberRepo crewRepo,//The repository for managing CrewMember entities
             IFlightCrewRepo flightCrewRepo, //The repository for managing FlightCrew (the join table) entities
-            IAircraftMaintenanceRepo maintenanceRepo
+            IAircraftMaintenanceRepo maintenanceRepo//The repository for managing AircraftMaintenance entities
         )
         {
 
